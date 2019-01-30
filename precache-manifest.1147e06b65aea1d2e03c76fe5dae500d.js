3a64b57ef65948a09e911f7e5029be4e2e63433d
kaichiro <kcfg23@gmail.com>
2019-01-30 09:15:42 -0200

    Updates

---
+++ precache-manifest.1147e06b65aea1d2e03c76fe5dae500d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "7e30cbedcce80908ac41",
+    "url": "/react-tutorial-npx/static/css/main.880ec47f.chunk.css"
+  },
+  {
+    "revision": "7e30cbedcce80908ac41",
+    "url": "/react-tutorial-npx/static/js/main.7e30cbed.chunk.js"
+  },
+  {
+    "revision": "f4789f78bc5099d1ff6c",
+    "url": "/react-tutorial-npx/static/js/1.f4789f78.chunk.js"
+  },
+  {
+    "revision": "774747a33d423f29a743",
+    "url": "/react-tutorial-npx/static/js/runtime~main.774747a3.js"
+  },
+  {
+    "revision": "b3058442b68cd01c4617d68e8ce1cb1b",
+    "url": "/react-tutorial-npx/index.html"
+  }
+];
