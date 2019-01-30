5b7b98874e3f0c60e524953f153d0812e384fe1a
kaichiro <kcfg23@gmail.com>
2019-01-30 08:31:21 -0200

    Initial commit

---
+++ src/components/Form.js
@@ -0,0 +1,49 @@
+import React, { Component } from "react";
+
+class Form extends Component {
+  constructor(props) {
+    super(props);
+
+    this.initialState = { name: "", job: "" };
+    this.state = this.initialState;
+  }
+
+  handleChange = event => {
+    const { name, value } = event.target;
+    this.setState({ [name]: value });
+  };
+
+  submitForm = () => {
+    this.props.handleSubmit(this.state);
+    this.setState(this.initialState);
+  };
+
+  render() {
+    const { name, job } = this.state;
+    return (
+      <section>
+        <form>
+          <label>Name</label>
+          <input
+            type="text"
+            name="name"
+            value={name}
+            onChange={this.handleChange}
+          />
+          <br />
+          <label>Job</label>
+          <input
+            type="text"
+            name="job"
+            value={job}
+            onChange={this.handleChange}
+          />
+          <br />
+          <input type="button" value="Submit" onClick={this.submitForm} />
+        </form>
+      </section>
+    );
+  }
+}
+
+export default Form;
